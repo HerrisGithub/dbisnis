3a608842c839c9787210f680f9b2ba775c2f17ec
Herris Suhendra <Herris Suhendra>
2018-01-09 22:46:22 +0700

    delete knex

--- knexfile.js
+++ knexfile.js
@@ -12,36 +12,8 @@
       tableName: 'migrations'
     },
     ssl:true,
-  //   client: 'pg',
-  //   connection: {
-  //     port:8989,
-  //     database:'direktoribisnis',
-  //     user:'postgres',
-  //     password:'postgres',
-  //     host:'127.0.0.1'},
-  // }
   }
 
-  // var production= {
-  //   client: 'postgresql',
-  //   connection: process.env.DATABASE_URL,
-  //   migrations: {
-  //     tableName: 'migrations'
-  //   },
-  //   ssl:true
-  // }
-  // var production= {
-  //   client: 'postgresql',
-  //   connection: {
-  //     user:process.env.SQL_USER,
-  //     password:process.env.SQL_PASSWORD,
-  //     database:process.env.SQL_DATABASE
-  //   },
-  //   migrations: {
-  //     tableName: 'migrations'
-  //   },
-  //   ssl:true
-  // }
   var knex=require('knex')(development);
 
 module.exports = {
