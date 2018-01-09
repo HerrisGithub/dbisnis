f77fa7c165edcfd675a5062ff1647db94d206a19
Herris Suhendra <Herris Suhendra>
2018-01-09 22:32:28 +0700

    'sd'

--- knexfile.js
+++ knexfile.js
@@ -45,7 +45,19 @@
   var knex=require('knex')(development);
 
 module.exports = {
-  knex:knex,
+  knex:require('knex')({
+    client: 'pg',
+    connection: {
+      user:process.env.SQL_USER,
+      password:process.env.SQL_PASSWORD,
+      database:process.env.SQL_DATABASE,
+      host:'104.154.29.255'
+    },
+    migrations: {
+      tableName: 'migrations'
+    },
+    ssl:true,
+  }),
   // development:development,
   // production:production
 };
