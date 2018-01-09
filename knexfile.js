7d2df4f6f2bd9464e8b7b547a3348566d2f0afcf
Herris Suhendra <Herris Suhendra>
2018-01-09 21:27:10 +0700

    connection

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@
 
 var process = require('process');
   var config = {
-    user: 'postgres',
-    password: 'postgres',
-    database: 'direktoribisnis'
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE
   };
 
   if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
@@ -20,34 +20,34 @@ var process = require('process');
 
 
   // var development= {
-  //   client: 'postgresql',
-  //   connection: {
-  //     user:process.env.SQL_USER,
-  //     password:process.env.SQL_PASSWORD,
-  //     database:process.env.SQL_DATABASE,
-  //     port:8989
+  //   // client: 'postgresql',
+  //   // connection: {
+  //   //   user:process.env.SQL_USER,
+  //   //   password:process.env.SQL_PASSWORD,
+  //   //   database:process.env.SQL_DATABASE,
+  //   //   port:8989
+  //   // },
+  //   migrations: {
+  //     tableName: 'migrations'
   //   },
+  //   ssl:true,
+  //   client: 'pg',
+  //   connection: {
+  //     port:8989,
+  //     database:'direktoribisnis',
+  //     user:'postgres',
+  //     password:'postgres',
+  //     host:'127.0.0.1'},
+  // }
+
+  // var production= {
+  //   client: 'postgresql',
+  //   connection: process.env.DATABASE_URL,
   //   migrations: {
   //     tableName: 'migrations'
   //   },
   //   ssl:true
-  //   // client: 'pg',
-  //   // connection: {
-  //   //   port:8989,
-  //   //   database:'direktoribisnis',
-  //   //   user:'postgres',
-  //   //   password:'postgres',
-  //   //   host:'127.0.0.1'},
   // }
-
-  // // var production= {
-  // //   client: 'postgresql',
-  // //   connection: process.env.DATABASE_URL,
-  // //   migrations: {
-  // //     tableName: 'migrations'
-  // //   },
-  // //   ssl:true
-  // // }
   // var production= {
   //   client: 'postgresql',
   //   connection: {
@@ -60,19 +60,10 @@ var process = require('process');
   //   },
   //   ssl:true
   // }
-  var knex=require('knex')({
-    client: 'postgresql',
-    connection: config
-  });
+  var knex=require('knex')(config);
 
 module.exports = {
   knex:knex,
-  development:{
-    client: 'postgresql',
-    connection: config
-  },
-  production:{
-    client: 'postgresql',
-    connection: config
-  }
+  development:config,
+  production:config
 };
