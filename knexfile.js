47b5d87e8e0b46a24b3e9460a2c7a6730093db0b
Herris Suhendra <Herris Suhendra>
2018-01-09 23:28:13 +0700

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,15 @@
 // Update with your config settings.
 
-var config = {
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE,
-  host:'104.154.29.255'
-};
-if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-  config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
+// var config = {
+//   user: process.env.SQL_USER,
+//   password: process.env.SQL_PASSWORD,
+//   database: process.env.SQL_DATABASE,
+//   host:'104.154.29.255'
+// };
+// if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+//   config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+// }
+var config ='dirbis-414d9:us-central1:direktoribisnis';
 module.exports = {
 
   development: {
