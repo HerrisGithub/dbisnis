de62490c86523c56fb1ffdd22afb795411ef5f97
Herris Suhendra <Herris Suhendra>
2018-01-09 21:52:34 +0700

    knex

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,25 @@
 // Update with your config settings.
 
 var process = require('process');
-var Knex = require('knex');
-var config = {
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE
-};
+  var config = {
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE
+  };
 
-if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-  config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
+  if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+    config.socketPath = '/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}';
+  }
 
-module.exports = {
-  knex:require('knex')({
+  var knex = Knex({
     client: 'postgresql',
     connection: config
-  })
-  // development:knex,
-  // production:knex
+  });
+
+  var knex=require('knex')(knex);
+
+module.exports = {
+  knex:knex,
+  // development:development,
+  // production:production
 };
