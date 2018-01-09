11e817c9995b67fb3ac2fb6a4ac0419a51960ba7
Herris Suhendra <Herris Suhendra>
2018-01-09 21:47:49 +0700

    adsad

--- knexfile.js
+++ knexfile.js
@@ -2,27 +2,23 @@
 
 var process = require('process');
 var Knex = require('knex');
-var knex = connect();
-function connect () {
-  var config = {
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWORD,
-    database: process.env.SQL_DATABASE
-  };
+var config = {
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: process.env.SQL_DATABASE
+};
 
-  if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-    config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-  }
-
-  // Connect to the database
-  var knex = Knex({
-    client: 'pg',
-    connection: config
-  });
-
-  return knex;
+if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+  config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
 }
-  var conn=knex;
+
+// Connect to the database
+var knex = Knex({
+  client: 'pg',
+  connection: config
+});
+
+var conn=knex;
 
 module.exports = {
   knex:conn,
