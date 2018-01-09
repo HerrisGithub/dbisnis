c42604b400519352b8ab26a6cd16f0cb5c7407c7
Herris Suhendra <Herris Suhendra>
2018-01-09 23:06:03 +0700

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,8 @@
 var config = {
   user: process.env.SQL_USER,
   password: process.env.SQL_PASSWORD,
-  database: process.env.SQL_DATABASE
+  database: process.env.SQL_DATABASE,
+  host:'104.154.29.255'
 };
 if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
   config.host = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
